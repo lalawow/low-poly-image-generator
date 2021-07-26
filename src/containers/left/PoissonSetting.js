2ef41b0141f4d071f8fdd02c38450e12af89df1b
Wei <lalawow@gmail.com>
2021-07-27 00:08:29 +0800

    refactor: PoisonSetting hooks

--- src/containers/left/PoissonSetting.js
+++ src/containers/left/PoissonSetting.js
@@ -1,44 +1,42 @@
-import React, { Component } from 'react'
-import { connect } from "react-redux";
+import React from 'react'
+import { shallowEqual, useSelector, useDispatch } from "react-redux";
 import { setSetting, renderControl } from "../../store/actions";
-import { getSetting } from "../../store/selectors";
 import { Slider, Switch } from 'antd';
 import UploadImage from "../../components/upload-image"
 
-@connect(state => {
-    const setting = getSetting(state) || []
-    return {
-        setting
-    }
-})
-export default class PoissonSetting extends Component {
-    handleChange = (type, value) => {
-        this.props.dispatch(setSetting({ type, value }))
-        this.props.dispatch(renderControl(true))
+
+const PoissonSetting = ()=>{
+    const imgSetting = useSelector((store) => store[store.mode],shallowEqual)
+    const { space, size, filled, stroke } = imgSetting;
+    const dispatch = useDispatch()
+
+    const onAfterChange = (type,value)=>{
+        dispatch(setSetting({ type, value }));
+        dispatch(renderControl(true));
     }
 
-    render() {
-        const { space, size, filled, stroke } = this.props.setting
-        return (
-            <div>
-                <UploadImage />
-                <div className="setting-item">
-                    <h4>Dot Space</h4>
-                    <Slider defaultValue={space} min={0} max={20} onAfterChange={this.handleChange.bind(this, "space")} />
-                </div>
-                <div className="setting-item">
-                    <h4>Dot Size</h4>
-                    <Slider defaultValue={size} min={1} max={20} onAfterChange={this.handleChange.bind(this, "size")} />
-                </div>
-                <div className="setting-item">
-                    <h4>Fill Dots</h4>
-                    <Switch defaultChecked={filled} onChange={this.handleChange.bind(this, "filled")} />
-                </div>
-                <div className="setting-item">
-                    <h4>Stroke</h4>
-                    <Switch defaultChecked={stroke} onChange={this.handleChange.bind(this, "stroke")} />
-                </div>
+    return (
+        <div>
+            <UploadImage />
+            <div className="setting-item">
+                <h4>Dot Space</h4>
+                <Slider defaultValue={space} min={0} max={20} onAfterChange={onAfterChange.bind(this,'space')} />
             </div>
-        )
-    }
+            <div className="setting-item">
+                <h4>Dot Size</h4>
+                <Slider defaultValue={size} min={1} max={20} onAfterChange={onAfterChange.bind(this, "size")} />
+            </div>
+            <div className="setting-item">
+                <h4>Fill Dots</h4>
+                <Switch defaultChecked={filled} onChange={onAfterChange.bind(this, "filled")} />
+            </div>
+            <div className="setting-item">
+                <h4>Stroke</h4>
+                <Switch defaultChecked={stroke} onChange={onAfterChange.bind(this, "stroke")} />
+            </div>
+        </div>
+    )
 }
+
+export default PoissonSetting;
+
